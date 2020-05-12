fbd637c9fcc86b708315e3fb9d50168449c70ad8
Tim Sherry <tsherry@tjs-mbp-2.home>
2020-05-11 20:39:15 -0400

    added logic to add arrays to userPw arr

---
+++ script.js
@@ -0,0 +1,54 @@
+$('button').on('click', function () {
+
+    let pwLength = parseInt(prompt('How long do you want your password to be?'));
+    console.log(pwLength);
+
+
+    let userPw = [];
+
+    var upperCase = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
+
+    var lowerCase = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+
+    var nums = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
+
+    var specialChar = ['/', '?', '!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '-', '=', '+'];
+
+    if (!pwLength) {
+        alert('Please select a number between 8 and 128');
+        pwLength = prompt('How long do you want your password to be?');
+    }
+    else if (pwLength > 128 || pwLength < 8) {
+        alert('Sorry password must be between 8 & 128 characters');
+        pwLength = prompt('How long do you want your password to be?');
+    } else {
+
+        let uppersConfirm = confirm('Do you want your password to contain uppercase letters?');
+        let numbersConfirm = confirm('Do you want your password to contain numbers?');
+        let specialCharConfirm = confirm('Do you want your password to contain special characters?');
+
+
+        if (uppersConfirm) {
+            userPw = userPw.concat(upperCase);
+        }
+        else if (numbersConfirm) {
+            userPw = userPw.concat(nums)
+        }
+        else if (specialCharConfirm) {
+            userPw = userPw.concat(specialChar)
+        };
+
+        userPw = userPw.concat(lowerCase);
+
+        console.log(userPw);
+
+        for (var i = 0; i < pwLength; i++){
+
+        let randompW = userPw[Math.floor(Math.random() * userPw.length)]
+        $('.pwDisplay').append(randompW);
+
+        }
+        
+    }
+
+});
